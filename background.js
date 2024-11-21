737e5956c32087ff5f49b9d61838d4330c0cb162
communitywaltoxm1032 <139952989+communitywaltoxm1032@users.noreply.github.com>
2024-11-21 16:04:39 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,70 @@
+﻿chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == 'install'){
+        chrome.storage.local.set({
+            isnewtab: false,
+            isdefaultfont: false,
+            ischeckip: true,
+            ishideblock: true,
+            isblockmobile: false,
+            isnickid: true,
+            ispagination: true,
+            isdarkmode: false,
+            isrefresh: false,
+            refreshtime: "10"
+        })
+    } else if(details.reason == "update"){
+        chrome.storage.local.get('isblockmobile',(val) => {
+            if(val.isblockmobile === undefined){
+                chrome.storage.local.set({
+                    isblockmobile: false
+                });
+            }
+            if(val.isdarkmode === undefined){
+                chrome.storage.local.set({
+                    isdarkmode: false
+                });
+            };
+            if(val.ispagination === undefined) {
+                chrome.storage.local.set({
+                    ispagination: false
+                });
+            }
+        });
+    }
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(async () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostPrefix: 'gall.dcinside.com' },
+        }),
+      ],
+      actions: [
+        new chrome.declarativeContent.SetIcon({
+          imageData: {
+            16: await loadImageData('icon/icon_16.png'),
+            32: await loadImageData('icon/icon_32.png'),
+          },
+        }),
+        chrome.declarativeContent.ShowAction
+          ? new chrome.declarativeContent.ShowAction()
+          : new chrome.declarativeContent.ShowPageAction(),
+      ],
+    }]);
+  });
+  
+  async function loadImageData(url) {
+    const img = await createImageBitmap(await (await fetch(url)).blob());
+    const {width: w, height: h} = img;
+    const canvas = new OffscreenCanvas(w, h);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, w, h);
+    return ctx.getImageData(0, 0, w, h);
+  }
+
+/*chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+        /(http|https):\/\/gall.dcinside.com\/.+/.test(tab.url) && chrome.pageAction.show(tabId);       
+    }
+);*/
---
+++ popup.js
@@ -0,0 +1,60 @@
+﻿const optlist = ['newtab', 'defaultfont', 'checkip', 'hideblock', 'blockmobile', 'nickid', 'pagination', 'darkmode', 'refresh', 'refreshtime'];
+
+function saveOptions() {
+    var setting = {};
+
+    for (var i = 0; i < optlist.length; i++) {
+        if (optlist[i] == 'refreshtime') {
+            setting['refreshtime'] = document.getElementById(optlist[i]).value;
+        }
+        setting['is' + optlist[i]] = document.getElementById(optlist[i]).checked;
+        setting['ishideblock'] = true;
+    }
+
+    chrome.storage.local.set(setting, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+function loadOptions() {
+    chrome.storage.local.get(null, function(items) {
+        for (var i = 0; i < optlist.length; i++) {
+            if (optlist[i] == 'refreshtime') {
+                if (items['isrefresh'] == true) {
+                    document.getElementById('refresh').checked = true;
+                    document.getElementById('refreshtime').style.display = 'inline-block';
+                    document.getElementById('refreshtime').value = items['refreshtime'];
+                } else {
+                    document.getElementById('refresh').checked = false;
+                    document.getElementById('refreshtime').style.display = 'none';
+                }
+            } else if(optlist[i] == 'hideblock'){
+                document.getElementById('hideblock').checked = true;
+            } else if ('is' + optlist[i] in items) {
+                document.getElementById(optlist[i]).checked = items['is' + optlist[i]];
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var refresh = document.getElementById('refresh');
+
+    //refresh option
+    refresh.addEventListener("change", function() {
+        if (this.checked) {
+            document.getElementById('refreshtime').style.display = 'inline-block';
+        } else {
+            document.getElementById('refreshtime').style.display = 'none';
+        }
+    });
+
+    loadOptions();
+
+    document.getElementById('save').onclick = saveOptions;
+});
